910e2f862f59a9feda91b6668f3b30567bb96a09
Ruben Kooijman <rbtkooijman@gmail.com>
2016-05-04 16:37:15 +0200

    Added script to fill every bar independently with frame markers

--- Assets/Frank&Ruben/Scripts/SliderValues.cs
+++ Assets/Frank&Ruben/Scripts/SliderValues.cs
@@ -1,28 +1,49 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
- 
+
 public class SliderValues : MonoBehaviour 
 {
 
 	// Update is called once per frame
     void Update () {
 
+        // Round the values down to a whole number and palce them in variables to be used in the user interface
+
+        // // World value
+        float worldRounded = Mathf.Floor(GameObject.Find("ARCamera").GetComponent<TrackableList>().worldValue);
+
+        // // Geography value
+        // float geoRounded = Mathf.Floor(GameObject.Find("ARCamera").GetComponent<TrackableList>().geoValue);
+
+        // // History value
+        // float histRounded = Mathf.Floor(GameObject.Find("ARCamera").GetComponent<TrackableList>().histValue);
+
+        // // Nature value
+        // float natRounded = Mathf.Floor(GameObject.Find("ARCamera").GetComponent<TrackableList>().natValue);
+
+        // // Technology value
+        // float techRounded = Mathf.Floor(GameObject.Find("ARCamera").GetComponent<TrackableList>().techValue);
+
     	// Update values of the UI slider elements with the value of the amount of detected frame markers in 'TrackableList.cs'
         
-    	// World
+    	// VALUE: WORLD
+        // Place the world value in the world slider object
         GameObject.Find("WorldSlider").GetComponent<Slider>().value = GameObject.Find("ARCamera").GetComponent<TrackableList>().worldValue;
+
+        // Place the rounded world value in the world percentage text label
+        GameObject.Find("WorldPercentageLabel").GetComponent<Text>().text = worldRounded + "%";
    	
-        // Aardrijkskunde
-		// GameObject.Find("GeographySlider").GetComponent<Slider>().value = GameObject.Find("ARCamera").GetComponent<TrackableList>().geoValue;
+        // VALUE: GEOGRAPHY
+		GameObject.Find("GeoSlider").GetComponent<Slider>().value = GameObject.Find("ARCamera").GetComponent<TrackableList>().geoValue;
 
-    	// Geschiedenis
-		// GameObject.Find("HistorySlider").GetComponent<Slider>().value = GameObject.Find("ARCamera").GetComponent<TrackableList>().histValue;
+    	// VALUE HISTORY
+		GameObject.Find("HistSlider").GetComponent<Slider>().value = GameObject.Find("ARCamera").GetComponent<TrackableList>().histValue;
     
-		// Natuur
-		// GameObject.Find("NatSlider").GetComponent<Slider>().value = GameObject.Find("ARCamera").GetComponent<TrackableList>().natValue;
+		// VALUE: NATURE
+		GameObject.Find("NatSlider").GetComponent<Slider>().value = GameObject.Find("ARCamera").GetComponent<TrackableList>().natValue;
 
-		// Techniek
-		// GameObject.Find("TechSlider").GetComponent<Slider>().value = GameObject.Find("ARCamera").GetComponent<TrackableList>().techValue;
+		// VALUE: TECHNOLOGY
+		GameObject.Find("TechSlider").GetComponent<Slider>().value = GameObject.Find("ARCamera").GetComponent<TrackableList>().techValue;
     }
 }
--- Assets/Frank&Ruben/Scripts/TrackableList.cs
+++ Assets/Frank&Ruben/Scripts/TrackableList.cs
@@ -6,8 +6,15 @@ using Vuforia;
 
 public class TrackableList : MonoBehaviour {
  
-    // Create variable to store world value in
-    public float worldValue;
+    // Create public variables to store world and theme values in
+    public float worldValue = 0f;
+    public float geoValue = 0f; 
+    public float histValue = 0f;
+    public float natValue = 0f; 
+    public float techValue = 0f;
+
+    // Booleans to remember if individual markers have been tracked
+    public bool marker0Tracked, marker1Tracked, marker2Tracked, marker3Tracked, marker4Tracked, marker5Tracked, marker6Tracked, marker7Tracked, marker8Tracked, marker9Tracked, marker10Tracked, marker11Tracked; 
 
     // Update is called once per frame
     void Update () {
@@ -22,27 +29,141 @@ public class TrackableList : MonoBehaviour {
         Debug.Log ("List of trackables currently active (tracked): ");
         int numFrameMarkers = 0;
 
-		// Create variable to store increasing world value in
-		float valueCount = 10f;
-
         foreach (TrackableBehaviour tb in activeTrackables) {
             Debug.Log("Trackable: " + tb.TrackableName);
  
-            if (tb is MarkerBehaviour)
+            if (tb is MarkerBehaviour) {
                 numFrameMarkers++;
+            }
 
-                // Increase world value with every frame marker detected
-                valueCount += 7.5f;
-        }
- 
-        // If 'valueCount' is greater than 'worldValue', place world value count in the public 'worldValue' variable to be used by the interface
-       	if (worldValue < valueCount) {
-       		worldValue += 7.5f;
-       	}
+		    // Check individual markers and increase values if detected
+		    if (tb.TrackableName.Equals("FrameMarker0")) {
+
+		    	if (marker0Tracked == false) {
+		    		geoValue += 30f;
+		    		worldValue += 7.5f;
+
+		    		marker0Tracked = true;
+		    	}
+		    }
+
+		    else if (tb.TrackableName.Equals("FrameMarker1")) {
+
+		    	if (marker1Tracked == false) {
+		    		geoValue += 30f;
+		    		worldValue += 7.5f;
+
+		    		marker1Tracked = true;
+		    	}
+		    }
+		    
+		    else if (tb.TrackableName.Equals("FrameMarker2")) {
+
+		    	if (marker2Tracked == false) {
+		    		geoValue += 30f;
+		    		worldValue += 7.5f;
+
+		    		marker2Tracked = true;
+		    	}
+		    }
+
+		    else if (tb.TrackableName.Equals("FrameMarker3")) {
+
+		    	if (marker3Tracked == false) {
+		    		histValue += 30f;
+		    		worldValue += 7.5f;
+
+		    		marker3Tracked = true;
+		    	}
+		    }
+
+		    else if (tb.TrackableName.Equals("FrameMarker4")) {
+
+		    	if (marker4Tracked == false) {
+		    		histValue += 30f;
+		    		worldValue += 7.5f;
+
+		    		marker4Tracked = true;
+		    	}
+		    }
+
+		    else if (tb.TrackableName.Equals("FrameMarker5")) {
+
+		    	if (marker5Tracked == false) {
+		    		histValue += 30f;
+		    		worldValue += 7.5f;
+
+		    		marker5Tracked = true;
+		    	}
+		    }
+
+		    else if (tb.TrackableName.Equals("FrameMarker6")) {
+
+		    	if (marker6Tracked == false) {
+		    		natValue += 30f;
+		    		worldValue += 7.5f;
+
+		    		marker6Tracked = true;
+		    	}
+		    }
+
+		    else if (tb.TrackableName.Equals("FrameMarker7")) {
+
+		    	if (marker7Tracked == false) {
+		    		natValue += 30f;
+		    		worldValue += 7.5f;
+
+		    		marker7Tracked = true;
+		    	}
+		    }
+
+		    else if (tb.TrackableName.Equals("FrameMarker8")) {
+
+		    	if (marker8Tracked == false) {
+		    		natValue += 30f;
+		    		worldValue += 7.5f;
+
+		    		marker8Tracked = true;
+		    	}
+		    }
+
+		    else if (tb.TrackableName.Equals("FrameMarker9")) {
+
+		    	if (marker9Tracked == false) {
+		    		techValue += 30f;
+		    		worldValue += 7.5f;
+
+		    		marker9Tracked = true;
+		    	}
+		    }
+
+		    else if (tb.TrackableName.Equals("FrameMarker10")) {
+
+		    	if (marker10Tracked == false) {
+		    		techValue += 30f;
+		    		worldValue += 7.5f;
+
+		    		marker10Tracked = true;
+		    	}
+		    }
+
+		    else if (tb.TrackableName.Equals("FrameMarker11")) {
+
+		    	if (marker11Tracked == false) {
+		    		techValue += 30f;
+		    		worldValue += 7.5f;
+
+		    		marker11Tracked = true;
+		    	}
+		    }		    		    		    		        		    		    	       
+		}
 
        	// Display the number of detected markers, the value count and the translated world value in the console
         Debug.Log ("Found " + numFrameMarkers + " frame markers in current frame.");
-        Debug.Log ("Value count: " + valueCount);
-        Debug.Log ("World value: " + worldValue + "%");    
+        Debug.Log ("World value: " + worldValue + "%");
+        Debug.Log ("Geography value: " + geoValue + "%");
+        Debug.Log ("History value: " + histValue + "%");
+        Debug.Log ("Nature value: " + natValue + "%");
+        Debug.Log ("Technology value: " + techValue + "%");
     }
 }
