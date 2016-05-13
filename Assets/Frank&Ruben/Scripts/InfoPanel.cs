002d76850c09799cccdc41182a80b189503dd2cb
Ruben Kooijman <rbtkooijman@gmail.com>
2016-05-13 13:37:02 +0200

    Implemented info panel toggle animation

--- Assets/Frank&Ruben/Scripts/InfoPanel.cs
+++ Assets/Frank&Ruben/Scripts/InfoPanel.cs
@@ -6,24 +6,32 @@ public class InfoPanel : MonoBehaviour {
 	// Create boolean to remember if the info panel is open or not
 	public bool PanelOpened;
 
+	// Create reference to the animator component
+	private Animator anim;
+
+	void Update () {
+
+		anim = GetComponent<Animator>();
+	}
+
 	public void ToggleInfoPanel () {
 
 		if (PanelOpened == false) {
-			GameObject.Find("InfoPanel").GetComponent<RectTransform>().sizeDelta = new Vector2(990f, 1350f);	
+			// GameObject.Find("InfoPanel").GetComponent<RectTransform>().sizeDelta = new Vector2(990f, 1350f);	
 
-			// animation.Play("OpenInfoPanel");
+			anim.Play ("OpenInfoPanel");
 
-			PanelOpened = true;	
+			PanelOpened = true;
 		}
 
 		else {
-			GameObject.Find("InfoPanel").GetComponent<RectTransform>().sizeDelta = new Vector2(0f, 0f);
+			// GameObject.Find("InfoPanel").GetComponent<RectTransform>().sizeDelta = new Vector2(0f, 0f);
 
-			// animation.Play("CloseInfoPanel");
+			anim.Play ("CloseInfoPanel");
 
 			PanelOpened = false;
 		}
 
-
 	}
+
 }
